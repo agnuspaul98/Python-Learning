dffd2ec36a447bb682b312e7826f09dcb2a11e08
agnuspaul98 <agnuspaul98@gmail.com>
2024-03-13 14:13:39 +0000

    Coding Efficiency Practice

---
+++ TimeObservation/GREATERNUMBER.md
@@ -0,0 +1,11 @@
+# Learnings
+
+13/03/2024
+This was my first approach trying to learn about efficiency practices while coding in Python.
+I learnt the below points:
+- Built in functions are more effective than user defined codes (if-else) that does the same purpose.
+- max() does not work semantically, the arguments need to be different, i.e, if the arguments are equal, the function returns the first argument as the greater number 
+even if the inputs are equal.
+- While trying to arrange the time variables in ascending order, I came across Dictionaries as a more convenient way to portray which coding segment is the most 
+efficient, but it needs to be looked into later on (use Key-Value pairs).
+- If you give, x=print("blah blah blah"), this holds None as its value 
--- TimeObservation/GreaterNumber.py
+++ TimeObservation/GreaterNumber.py
@@ -1,26 +1,42 @@
 import time
 
 s1 = time.time()
-a,b=4,5
+a,b=5,5
 if(a>b):
     print("{} is greater than {}".format(a,b))
-else:
+elif(a<b):
     print("{} is greater than {}".format(b,a))
+else:
+    print("{} is equal to {}".format(a,b))
 
 e1=time.time()
+p1=(e1-s1) * 1000
 print ("The time of execution of above program is :",
-       (e1-s1) * 1000, "ms")
+       p1, "ms")
 
-s2=time.time()
+s2 = time.time()
+c,d=5,10
+print(max(c,d))
+e2=time.time()
+p2=(e2-s2) * 1000
+print ("The time of execution of above program is :",
+       p2, "ms")
+
+
+s3=time.time()
 i1=input("Enter first number:")
 i2=input("Enter second number:")
 if(i1>i2):
     print("{} is greater than {}".format(i1,i2))
-elif(i2<i1):
+elif(i1<i2):
     print("{} is greater than {}".format(i2,i1))
 else:
     print("{} is equal to {}".format(i1,i2))
 
-e2=time.time()
+e3=time.time()
+p3=(e3-s3)*1000
 print ("The time of execution of above program is :",
-       (e2-s2) , "s")
+       p3 , "ms")
+
+pList=[p1,p2,p3]
+print(sorted(pList))
