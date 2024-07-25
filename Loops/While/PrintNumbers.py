77d4d5b1aced8bee4c8f6d142c1c157d6b7d416e
agnuspaul98 <agnuspaul98@gmail.com>
2024-07-25 11:27:02 +0530

    while loop

---
+++ Loops/While/PrintNumbers.py
@@ -0,0 +1,6 @@
+# Print first 20 numbers
+
+i=0
+while(i<=20):
+    print(i)
+    i+=1
---
+++ Loops/While/PrintThrice.py
@@ -0,0 +1,8 @@
+# Program to write 'hello world' three times
+
+c="Hello World"
+a=3
+while (a>0):
+    print(c)
+    a-=1
+print("now outside while")
